21ae151b26e51c85f88a850bd5e7e5a5c998caa3
Vagner <vagner.leite@autoavaliar.com.br>
2019-11-12 16:53:57 -0300

    Ecma-script

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,5 +1,20 @@
-class TodoList {
+class List {
     constructor() {
-        
+        this.data = [];
     }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List {
+    
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo Todo');
 }
